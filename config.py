40d4b591a9b1a508ed3fbd9ba7ea9938614a084d
Weirdllama <26628341+weirdllama2@users.noreply.github.com>
2025-04-29 08:43:20 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Configuration file for the SHL Peak TPE Tracker
+
+API_URL = "https://portal.simulationhockey.com/api/v1"  # Base API URL
+
+OUTPUT_FILE = 'peak_tpe_rankings.csv'  # Output file where the rankings will be saved
